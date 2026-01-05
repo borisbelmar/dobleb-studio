435577fd628ba9b6bee409fb725e6599f66e996f
Boris Belmar <borisbelmarm@gmail.com>
2026-01-04 23:49:43 -0300

    feat: update appId in CLI config to match the current deployment settings

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -11,6 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
     autoUpdates: true,
-    appId: 'dobleb'
+    appId: 'wjek15nmsznplt93jgfezxom'
   }
 })
